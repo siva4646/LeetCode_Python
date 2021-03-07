64def922650942c2b2208187c392518fed6735e6
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-07 09:12:15 -0600

    Time: 36 ms (50.34%), Space: 13.5 MB (52.31%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) == None:
+            return []
+        
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[target - nums[i]] = i
+            else:
+                return dic[nums[i]], i
+            
+            
+            
+            
+            
+    
+        
