8f1c44fe3aab336e06023746d0ee0d1dd6969c63
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-07 21:15:54 -0600

    Time: 36 ms (83.29%), Space: 14.6 MB (14.49%) - LeetHub

--- remove-k-digits/remove-k-digits.py
+++ remove-k-digits/remove-k-digits.py
@@ -2,18 +2,29 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         if not num or len(num) == 0:
             return ""
-        
+             
         stack = []
         for i in range(len(num)):
             while k and stack and stack[-1] > num[i]:
                 stack.pop()
                 k -= 1
             stack.append(num[i])
-        res = ''.join(stack[:len(stack)-k]).lstrip('0')
-        if len(res) != 0:
-            return res
+        result = "".join(stack[:len(stack)-k]).lstrip('0')
+        
+        if len(result) != 0:
+            return result
         else:
-            return '0'
+            return "0"
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
