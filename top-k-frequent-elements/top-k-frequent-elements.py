1023745dc42e8b87c6afed0936cd090114d62b05
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-08 17:30:58 -0600

    Time: 104 ms (54.00%), Space: 18.9 MB (28.53%) - LeetHub

---
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if not nums or len(nums) == 0:
+            return []
+        
+        maxHeap = []
+        result = []
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+        
+        for key, value in dic.items():
+            heapq.heappush(maxHeap, (-value, key))
+        
+        while k != 0:
+            result.append(heapq.heappop(maxHeap)[1])
+            k -= 1
+        return result
+         
