a853253b484f660796e6fd756f6df8ef85d65efa
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-11 09:07:43 -0600

    Time: 48 ms (84.72%), Space: 14.5 MB (41.33%) - LeetHub

--- kth-missing-positive-number/kth-missing-positive-number.py
+++ kth-missing-positive-number/kth-missing-positive-number.py
@@ -4,9 +4,8 @@ class Solution:
         high = len(arr) - 1
         
         while low <= high:
-            mid = (low + high) // 2
-            
-            if arr[mid] - mid -1 < k:
+            mid = low + (high - low) // 2
+            if arr[mid] - mid - 1 < k:
                 low = mid + 1
             else:
                 high = mid - 1
