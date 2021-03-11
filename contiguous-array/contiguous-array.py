0f5ea40f1b9e1bbb74dc7ec80159465d90d6a01a
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-11 12:08:45 -0600

    Time: 836 ms (87.94%), Space: 18.7 MB (71.33%) - LeetHub

---
+++ contiguous-array/contiguous-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        dic = {0: -1}
+        rSum = 0
+        lenthSubArray = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                rSum -= 1
+            else:
+                rSum += 1
+            
+            if rSum in dic:
+                lenthSubArray = max(lenthSubArray, i - dic[rSum])
+            else:
+                dic[rSum] = i
+        return lenthSubArray
+        
+          
