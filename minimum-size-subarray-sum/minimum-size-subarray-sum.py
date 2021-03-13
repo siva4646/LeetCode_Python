63867d0f1a72d2af5894c7f77c0979f54e2870db
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-13 08:33:59 -0600

    Time: 100 ms (21.84%), Space: 16.6 MB (48.91%) - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,29 +1,50 @@
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        #Sliding Window 
         left = 0
         right = 0
         rSum = 0
-        rLen = float('inf')
+        rLen = float("inf")
         
         while left < len(nums):
-            # Growth Phase
-            if rSum < s:
-                rSum += nums[left]
+            if rSum <= s:
+                rSum = rSum + nums[left]
                 left += 1
-            # Shrink Phase
+            
             while rSum >= s:
-                if left - right  < rLen:
+                if left - right <= rLen: 
                     rLen = left - right
-                    print(rLen)
-                else:
-                    rSum -= nums[right]
-                    right += 1
-                  
-        if rLen == float('inf'): # not reached it says we are just growing phase which rlen never change
+                    
+                rSum = rSum - nums[right]
+                right += 1
+
+        if rLen == float("inf"):
             return 0
         else:
             return rLen
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
 #https://www.youtube.com/watch?v=SeJgBWg1_w4&ab_channel=Brevity-CodeIntuitively
 
