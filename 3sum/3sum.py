9d4c95da0d9cd5ba9acae44c6bacb0c010c3407d
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-13 20:03:30 -0600

    Time: 792 ms (82.22%), Space: 17.5 MB (52.04%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -2,9 +2,11 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
         nums.sort()
-    
-        for i in range(len(nums) - 1):
-            if i > 0 and nums[i] == nums[i- 1]:
+        
+        for i in range(len(nums) - 2):
+            # it should not contain duplicate triplet
+            # [-3, -3, 1,2,3,4] since -3 once visited so we will skip it continue or else we will lead same triplet
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             left = i + 1
             right = len(nums) - 1
@@ -17,16 +19,17 @@ class Solution:
                     right -= 1
                 else:
                     result.append([nums[i], nums[left], nums[right]])
-                    # to get unique tripet
-                    while left < right and nums[left] == nums[left + 1]:
-                        left = left + 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right = right - 1
-                    
-                    left = left + 1
-                    right = right - 1
+                    # print(result)
+                    # [-2, -2 , 0, 0,2,2] we don't have to have same number so left += 1
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1 # right pointer will take care in first while loop
+                        #[-2, -2 , 0, 0,2,2]
                     
         return result 
+    
+# https://www.youtube.com/watch?v=jzZsG8n2R9A&ab_channel=NeetCode
+# 
                     
         
         
