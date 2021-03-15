7157c244be93d1df71c2cdce3862f7c7261ef366
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-15 14:05:13 -0500

    Time: 1040 ms (93.62%), Space: 25.2 MB (42.02%) - LeetHub

--- minimum-deletion-cost-to-avoid-repeating-letters/minimum-deletion-cost-to-avoid-repeating-letters.py
+++ minimum-deletion-cost-to-avoid-repeating-letters/minimum-deletion-cost-to-avoid-repeating-letters.py
@@ -1,19 +1,15 @@
 class Solution:
     def minCost(self, s: str, cost: List[int]) -> int:
-        left = 0
-        result = 0
-        
-        while left < len(s):
-            right = left + 1
-            maxCost = cost[left]
-            curr = cost[left]
-            
-            while right < len(s) and s[left] == s[right]:
-                maxCost = max(maxCost, cost[right])
-                curr += cost[right]
-                right += 1
-                
-            result += curr - maxCost
-                
-            left= right
+        if not s or not cost:
+            return 0
+        slow=0
+        result=0
+        l=len(s)
+        for j in range(1,l+1):
+            if j>=l or s[j]!=s[slow]:
+                if j-slow>1:
+                    sum_=sum(cost[slow:j])
+                    max_=max(cost[slow:j])
+                    result+=sum_-max_
+                slow=j
         return result
