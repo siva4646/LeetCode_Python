3e6af84965a6b6af07d3e022fe57ede73a13f21a
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-15 07:22:13 -0500

    Time: 52 ms (21.48%), Space: 14.4 MB (28.44%) - LeetHub

--- partition-labels/partition-labels.py
+++ partition-labels/partition-labels.py
@@ -4,13 +4,15 @@ class Solution:
         result = []
         for i in range(len(S)):
             dic[S[i]] = i
-       
+        print(dic)
+            
         start_idx = 0
         end_idx = 0
-        
+ 
         for i in range(len(S)):
             end_idx = max(end_idx, dic[S[i]])
+            print(end_idx)
             if i == end_idx:
                 result.append(i - start_idx + 1)
                 start_idx = i + 1
-        return result
+        return result 
