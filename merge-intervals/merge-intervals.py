8cc2f207a9a27c79deb2d3da33154cdd5af920b9
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-17 10:38:13 -0500

    Time: 96 ms (29.42%), Space: 15.9 MB (97.54%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -3,7 +3,7 @@ class Solution:
         if not intervals or len(intervals) == 0:
             return []
         result = []
-        intervals.sort()
+        intervals.sort(key = lambda x:x[0])
         for interval in intervals:
             if result and interval[0] <= result[-1][1]:
                 result[-1][1] = max(result[-1][1], interval[1])
