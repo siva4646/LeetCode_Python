cc38dd521ac5daea3ea999e47e0409e5b3797ae3
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-18 20:18:00 -0500

    Time: 212 ms (44.63%), Space: 24 MB (11.65%) - LeetHub

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -4,79 +4,53 @@ class Node:
         self.val = val
         self.next = None
         self.prev = None
-        
-#https://www.youtube.com/watch?v=S6IfqDXWa10&ab_channel=BackToBackSWE
-
-class LRUCache(object):
-    
-    #https://www.youtube.com/watch?v=akFRa58Svug&ab_channel=TECHDOSE 
-
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+class LRUCache:
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {} # as dic
-        self.head = Node(-1, -1) #node is called it automatically call init(which has key, val, next prev) as in constructor will be called automatically when the object is created.
-        self.tail = Node(-1, -1) # set tail as -1 -1
+        self.dic = {}
+        
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
         self.head.next = self.tail
-        self.tail.prev = self.head   # make a connection
-        
-
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        result = -1
-        if key in self.cache: # check in dic
-            node = self.cache[key] # take key in node go to the key - " get address in node that is O(1)memory allocation" add key memory allocation to node so that I will get instant access of the value
-            result = node.val # add node value in res 
-            self.remove(node) #it's used remove and add it to the front of the head that is MRU
-            self.add(node)
-            
-        return result
-        
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        if key in self.cache:  #if key in dic
-            node = self.cache[key] # in cache
-            node.val = value # just update the value in node 
-            self.remove(node) # remove since it's used recently 
-            self.add(node) # add at the front (most recently used)
-            
-        elif len(self.cache) == self.capacity: # fit to the size if it's full
-            node = self.tail.prev # remove lRC element
-            self.remove(node) # Remove that node
-            del self.cache[node.key] # delete in cache
-            new_node = Node(key, value)  # create a new 
-            self.add(new_node) # add new node next to head
-            self.cache[key] = new_node # add in cache as well
+        self.tail.prev = self.head
+    
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        node = self.dic[key]
+        self.remove(node)
+        self.addToHead(node)
+        return node.val
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            node.val = value
+            self.remove(node)
+            self.addToHead(node)
             
         else:
-            new_node = Node(key, value) # nothing just add at the front (head)
-            self.add(new_node) 
-            self.cache[key] = new_node
-        
-    
-    def remove(self, node): 
-        node.next.prev = node.prev
-        node.prev.next = node.next
-    
-    def add(self, node): #most recently used                                                                                                   
+            if len(self.dic) == self.capacity:
+                tailPrev = self.tail.prev
+                self.remove(tailPrev)
+                del self.dic[tailPrev.key]
+                
+            newNode = Node(key, value)
+            self.addToHead(newNode)
+            self.dic[key] = newNode   
+
+    def addToHead(self, node): # most recently used
         node.prev = self.head
         node.next = self.head.next
         self.head.next = node
         node.next.prev = node
         
         
-        
-    
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+         
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
