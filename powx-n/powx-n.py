b3673aa3251fb73b8e6e99981323043cc04ccc00
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-22 13:14:53 -0500

    Time: 24 ms (95.91%), Space: 14.4 MB (20.62%) - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -4,12 +4,16 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         # Base
         if (n == 0):
-            return 1
+            return 1 
         
+        # Logic
         temp = self.myPow(x, int(n/2))
+         # if n is even
         if n % 2 == 0:
             return temp * temp
+        # if n is -ve and odd
         elif n < 0:
             return temp * temp * (1/x)
+         # if n is odd
         else:
             return temp * temp * x
