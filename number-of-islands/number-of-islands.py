0f0742c9fccce1a7f9fcf6caaaab71575765aec3
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-23 20:16:51 -0500

    Time: 152 ms (41.54%), Space: 15.4 MB (72.32%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -3,23 +3,24 @@ class Solution:
         self.m = len(grid)
         self.n = len(grid[0])
         count = 0
+        
         for i in range(self.m):
             for j in range(self.n):
                 if grid[i][j] == "1":
-                    self.dfs(grid, i, j)
                     count += 1
+                    self.dfs(grid, i, j)
         return count
+    
     def dfs(self, grid, i, j):
         # Base
         if i < 0 or j < 0 or i == self.m or j == self.n or grid[i][j] != "1":
             return 
-        
-        # logic
+        # Logic
         grid[i][j] = "X"
-        directions = [(-1,0), (1,0), (0,1), (0,-1)]
-        for dirs in directions:
+        direction = [(-1,0), (1,0), (0,1), (0,-1)]
+        for dirs in direction:
             r = dirs[0] + i
             c = dirs[1] + j
-            self.dfs(grid, r,c)
+            self.dfs(grid, r, c)
         
         
