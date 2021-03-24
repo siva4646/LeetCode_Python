3e5e753aa2510121de487cc64746d3ae49e7de20
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-24 09:39:18 -0500

    Time: 104 ms (44.50%), Space: 15.7 MB (56.36%) - LeetHub

--- course-schedule-ii/course-schedule-ii.py
+++ course-schedule-ii/course-schedule-ii.py
@@ -1,33 +1,33 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         indegree = [0] * numCourses
-        dic = collections.defaultdict(list)
-        queue = deque()
         result = []
+        queue = deque()
+        dic = collections.defaultdict(list)
         
         for course, prereq in prerequisites:
             dic[prereq].append(course)
             indegree[course] += 1
+        print(dic)
             
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
-        # no independent node initally cannot take any course in initally phase        
                 
         while queue:
             current_course = queue.popleft()
             result.append(current_course)
-          
+            
             edge = dic[current_course]
             
-            for nextcourse in edge:
-                indegree[nextcourse] -= 1
+            for newcourse in edge:
+                indegree[newcourse] -= 1
                 
-                if indegree[nextcourse] == 0:
-                    queue.append(nextcourse)
+                if indegree[newcourse] == 0:
+                    queue.append(newcourse)
                     
+        
         if len(result) == numCourses:
             return result
         else:
             return []
-        
