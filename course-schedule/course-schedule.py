43954e54d137947338f53858a3feac165283993c
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-24 17:41:15 -0500

    Time: 76 ms (82.37%), Space: 14.6 MB (99.48%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,44 +1,33 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Base condition
-        if not prerequisites or len(prerequisites) == 0:
-            return True
-        
-        # Maintain indegree array since we need to know the independent node
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
         indegree = [0] * numCourses
+        count_course = 0
         queue = deque()
-        course_count = 0
-        # Maintain adjacency list
         dic = collections.defaultdict(list)
         
-        # fill my adjacency list key as prereq and val as course and increment my indegree of course to 1
-        for course, prereq in prerequisites:
+        for prereq, course in prerequisites:
             dic[prereq].append(course)
-            indegree[course] +=1
-        print(indegree)
+            indegree[course] += 1
         
-        # if indegree[i] is zero once we process then add to the queue
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
-        
-        if not queue:
-            return False
-        # process queue 
+                
+        # if not queue:
+        #     return False
         while queue:
-            current_course = queue.popleft()
-            # increment since it can be taken
-            course_count += 1
+            curr_course = queue.popleft()
+            count_course += 1
+            edge = dic[curr_course]
             
-            # search in adjacency list and store it's value in edge
-            edge = dic[current_course]
+            for newCourse in edge:
+                indegree[newCourse] -= 1
+                
+                if indegree[newCourse] == 0:
+                    queue.append(newCourse)
+
+        return count_course == numCourses
+       
+        
             
-            # iterate through it and decrement indegree since it can be taken and if indegree[nextcourse] == 0 then add to the queue to process again
-            for nextcourse in edge:
-                indegree[nextcourse] -= 1
-            
-                if indegree[nextcourse] == 0:
-                    queue.append(nextcourse)
-                    
-        return course_count == numCourses
-    
+        
