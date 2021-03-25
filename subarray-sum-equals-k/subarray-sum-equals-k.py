d90e0542ad2eda4de74d56168b44e98d8f1bdaad
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-25 18:53:32 -0500

    Time: 204 ms (95.13%), Space: 16 MB (74.04%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,21 +1,23 @@
-
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        #running sum
+class Solution(object):
+    def subarraySum(self, nums, k):
         if not nums or len(nums) == 0:
             return 0
         
-        dic = {0: 1}
         count = 0
         rSum = 0
+        dic = {0:1}
+        
         for i in range(len(nums)):
-            rSum += nums[i]
-            compliment = rSum - k
-            if compliment in dic:
-                count += dic[compliment]
-               
+            rSum = rSum + nums[i]
+            compliement = rSum - k
+            
+            if compliement in dic:
+                count += dic[compliement]
+                
             if rSum not in dic:
                 dic[rSum] = 1
             else:
                 dic[rSum] += 1
+                
         return count
+        
