d045396d52de0a48144c9b5d47329c0255d1a185
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-24 20:15:27 -0500

    Time: 20 ms (98.17%), Space: 13.8 MB (41.72%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -3,25 +3,24 @@ class Solution(object):
         if not nums or len(nums) == 0:
             return -1
         
-        left = 0
-        right = len(nums) - 1
+        low = 0
+        high = len(nums) - 1
         
-        while left <= right:
-            mid = left+ (right - left) // 2
+        while low <= high:
+            mid = low +(high - low) // 2
             
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
             
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1 
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
                 else:
-                    left = mid + 1
+                    low = mid + 1
             else:
-                if nums[right] >= target >= nums[mid]:
-                    left = mid + 1
+                if nums[high] >= target >= nums[mid]:
+                    low = mid + 1
                 else:
-                    right = mid - 1
+                    high = mid - 1
+        return -1
                     
-        return -1 
-        
