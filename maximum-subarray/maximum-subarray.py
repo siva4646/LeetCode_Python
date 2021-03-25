5bddbb0a94d0063555650a740088a84d61d44ccb
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-25 18:29:11 -0500

    Time: 44 ms (87.95%), Space: 13.9 MB (95.31%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,29 +1,19 @@
-# sliding window rSum
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         if not nums or len(nums) == 0:
             return 0
-        rSum = nums[0]
-        maxi = nums[0] # gobal array 
         
-        start_idx = 0
-        end_idx = 0
-        startCurr = 0
+        rSum = nums[0]
+        maxi = nums[0]
         
         for i in range(1, len(nums)):
-            rSum = rSum + nums[i]
-            if rSum <= nums[i]:
-                rSum = nums[i] #   rSum = max(rSum + nums[i], nums[i]) same doing this & above line
-                startCurr = i
-                # print(startCurr)
-                #print("first", rSum, startCurr)
+            #rSum = rSum + nums[i]
+            rSum = max( rSum + nums[i], nums[i])
+            maxi = max(maxi, rSum)
             
-            if maxi <= rSum:
-                maxi = rSum # maxi = max(maxi, rSum) same doing this and above line
-                start_idx = startCurr
-                print(start_idx)
-                end_idx = i
-                #print("second", maxi, start_idx, end_idx)
-                
-        print(start_idx, end_idx)
         return maxi
+            
