353e2d5fdb8960978a00a03fb2df1f6549899658
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-25 13:31:58 -0500

    Time: 32 ms (95.01%), Space: 13.4 MB (96.88%) - LeetHub

--- wildcard-matching/wildcard-matching.py
+++ wildcard-matching/wildcard-matching.py
@@ -1,34 +1,30 @@
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
+class Solution(object):
+    def isMatch(self, s, p):
         if s == p or p == "*":
-            return True # Ex: 2
-        sp , pp = 0,  0
-        sStar, pStar = -1, -1
+            return True
+        sStar, pStar= -1, -1
+        sp, pp = 0, 0
         
         while sp < len(s):
-            #case 1 
-            if pp < len(p) and (s[sp] == p[pp] or p[pp] == "?"): # since increase pp+ = 1 check pp < pl 
+            # case 1
+            if pp < len(p) and (s[sp] == p[pp] or p[pp] == '?'):
                 sp += 1
                 pp += 1
-            elif pp < len(p) and p[pp] == "*": 
-                 # Zero case
-                sStar = sp
+            elif pp < len(p) and p[pp] == "*": # Zero case
                 pStar = pp
+                sStar = sp
                 pp += 1
             elif pStar == -1:
                 return False
-            else:  # One case
+            else:
                 sStar += 1
-                sp = sStar # we skipped the char sStar or we cleverly matched that char to our *
+                sp = sStar
                 pp = pStar + 1
                 
         while pp < len(p):
-            if p[pp] != '*':
+            if p[pp] != "*":
                 return False
             pp += 1
         return True
-            
-                
-                
             
         
