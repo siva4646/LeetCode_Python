65e2c265b4d238a1ff736c6dc7732751dc719235
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-25 20:26:55 -0500

    Time: 68 ms (51.09%), Space: 15.9 MB (20.58%) - LeetHub

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,16 +1,17 @@
-class Solution:
-    def findPairs(self, nums: List[int], k: int) -> int:
-        count_val = Counter(nums)
-        print(count_val)
-        count = 0
-        for key, value in count_val.items():
+class Solution(object):
+    def findPairs(self, nums, k):
+        count = Counter(nums)
+        c_count = 0
+        
+        for key, value in count.items():
             if k == 0:
                 if value > 1:
-                    count += 1
+                    c_count += 1
             else:
-                total_val = key + k
-                if total_val in count_val:
-                    count += 1
+                total = key + k
+                if total in count:
+                    c_count += 1
+        return c_count 
                     
-        return count
+                
         
