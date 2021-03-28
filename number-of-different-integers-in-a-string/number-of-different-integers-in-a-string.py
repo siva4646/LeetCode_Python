e41a85d430df603a3fca769bdeff16e2bc8ad8ca
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-28 09:16:14 -0500

    Time: 16 ms (100.00%), Space: 13.5 MB (100.00%) - LeetHub

--- number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.py
+++ number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.py
@@ -1,46 +1,16 @@
-import re
-class Solution:
-    def numDifferentIntegers(self, word: str) -> int:
-        nums, token = set(), ''
+class Solution(object):
+    def numDifferentIntegers(self, word):
+        nums, token = set(), ""
         
-        for i, l in enumerate(word):
-            if token != '' and l.isalpha():
+        for i, char in enumerate(word):
+            if token != "" and char.isalpha():
                 nums.add(int(token))
-                print(nums)
-                token = ''
-            elif l.isnumeric():
-                token += l
+                token = ""
+            elif char.isdigit():
+                token += char
                 
-            if i == len(word) - 1 and l.isnumeric():
+            if i == len(word) - 1 and char.isdigit():
                 nums.add(int(token))
-               
-                    
-        return len(nums)        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         hashset = set()
-#         each_num = ""
-#         left = 0
-#         right = 0
-#         while left < len(word):
-#             if word[left].isdigit() or word[right].isdigit():
-#                 each_num += word[left]
-#                 right += 1
-#             hashset.add(each_num)
             
-#             left += 1
-#             right += 1
-#         print(hashset)
+        return len(nums)
         
