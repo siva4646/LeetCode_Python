878c21e8e21078f6b4174cfc30110ad96ad4f305
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-03-31 14:35:34 -0500

    Time: 36 ms (6.85%), Space: 13.5 MB (65.98%) - LeetHub

--- binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -1,51 +1,39 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
-        # Instead of sort we can maintain max and min then iterate through it.
-        
-        result = []
-        
-        if root == None:
-            return result
-        
+class Solution(object):
+    def verticalOrder(self, root):
+        if not root:
+            return []
         queue = deque()
-        queue_col = deque()
-        hashmap = collections.defaultdict(list)
-        mini = 0
+        queue.append((root, 0))
         maxi = 0
-        queue.append(root)
-        queue_col.append(0)
-        
-        while queue: # Size variable
-            curr = queue.popleft()
-            col = queue_col.popleft()
-            
-            if col not in hashmap:
-                hashmap[col] = []
+        mini = 0
+        result = []
+        hashmap = collections.defaultdict(list)
     
-            hashmap[col].append(curr.val)
-            #print(hashmap)
-            # left child
+        while queue:
+            curr, level = queue.popleft()
+            
+            mini = min(mini, level)
+            maxi = max(maxi, level)
+            
+            if level not in hashmap:
+                hashmap[level] = []
+            hashmap[level].append(curr.val)
+            print(hashmap)
+            
             if curr.left != None:
-                queue.append(curr.left)
-                queue_col.append(col - 1)
-                mini = min(mini, col-1)
-                #print(mini)
-            # right child
+                queue.append((curr.left, level - 1))
+                
             if curr.right != None:
-                queue.append(curr.right)
-                queue_col.append(col + 1)
-                maxi = max(maxi, col+1)
-                #print(maxi)
-        for val in range(mini, maxi+1):
-            #print("value-->", val)
+                queue.append((curr.right, level + 1))
+                
+        for val in range(mini, maxi + 1):
             result.append(hashmap[val])
-        
-        #print(result)
+            
         return result
         
