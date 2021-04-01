0d7a4a320648e6462e12195d5bd7e027f3f18019
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-01 18:13:47 -0500

    Time: 72 ms (42.79%), Space: 15.2 MB (76.45%) - LeetHub

--- jump-game/jump-game.py
+++ jump-game/jump-game.py
@@ -1,20 +1,15 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        max_dest = float("-inf")
+class Solution(object):
+    def canJump(self, nums):
+        maxdest = float("-inf")
         pos = 0
         
         for i in range(len(nums) - 1):
-            # calculate the max dest from that index
-            max_dest = max(max_dest, i + nums[i])
+            maxdest = max(maxdest, i + nums[i])
             
-            # will get with min jump to reach destination
             if pos == i:
-                pos = max_dest
-        
-        if pos >= len(nums) -1:
+                pos = maxdest
+                
+        if pos >= len(nums) - 1:
             return True
         return False
-        
-        
-        
         
