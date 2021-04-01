71dd8812d2c69a98fce7489826962cc3b6064ec9
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-01 18:34:50 -0500

    Time: 32 ms (84.67%), Space: 14.1 MB (89.99%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
-        max_dest = float("-inf")
+        maxdest = float("-inf")
         pos = 0
         jump = 0
-        for i in range(len(nums)-1):
-            max_dest = max(max_dest, i + nums[i])
+        for i in range(len(nums) - 1):
+            maxdest = max(maxdest, i + nums[i])
             
             if pos == i:
-                pos = max_dest
+                pos = maxdest
                 jump += 1
-        
-        return jump
+                
+        if pos >= len(nums)- 1:
+            return jump
+        return 0
         
