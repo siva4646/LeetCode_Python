8a9b6304d5ac4cca3f99364b47caf07eaac39c45
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-06 14:29:12 -0500

    Time: 32 ms (60.60%), Space: 14.4 MB (8.87%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,18 @@
+# Understand the Recursive call clearly in video!
+class Solution:
+    def fib(self, N: int) -> int:
+        cache = {}
+        def recur_fib(N):
+            if N in cache:
+                return cache[N]
+
+            if N < 2:
+                result = N
+            else:
+                result = recur_fib(N-1) + recur_fib(N-2)
+
+            # put result in cache for later reference.
+            cache[N] = result
+            return result
+
+        return recur_fib(N)   
