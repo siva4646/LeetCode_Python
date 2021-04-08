57e9a29670f278275e3b8ff20f8b3c2cc56904a4
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-08 07:57:53 -0500

    Time: 1740 ms (27.98%), Space: 13.7 MB (43.67%) - LeetHub

--- find-the-celebrity/find-the-celebrity.py
+++ find-the-celebrity/find-the-celebrity.py
@@ -1,14 +1,16 @@
 # The knows API is already defined for you.
-# return a bool, whether a knows b
-# def knows(a: int, b: int) -> bool:
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
 
-class Solution:
-    def findCelebrity(self, n: int) -> int:
+class Solution(object):
+    def findCelebrity(self, n):
         celebrity = 0
-        for person in range(1, n):
+        for person in range(1,n):
             if knows(celebrity, person):
                 celebrity = person
-                
+        
         for person in range(n):
             if person == celebrity:
                 continue
