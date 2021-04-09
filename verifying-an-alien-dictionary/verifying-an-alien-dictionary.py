1bf2e93852843aa0704d44cdbc7d1afe2380e6bc
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-09 17:08:34 -0500

    Time: 28 ms (23.68%), Space: 13.6 MB (52.13%) - LeetHub

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
+    def isAlienSorted(self, words, order):
         if not words or len(words) == 0:
             return True
         self.charMap = {}
@@ -7,10 +7,11 @@ class Solution:
         for i in range(26):
             self.charMap[order[i]] = i
             
+        print(self.charMap)
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i+1]
-            if self.isNotSorted(word1, word2):
+            if self.isNotSorted(word1, word2): # if this is true it will go inside the and return false as final output
                 return False
         return True
     
@@ -28,6 +29,4 @@ class Solution:
             p1 += 1
             p2 += 1
             
-        return len(word1) > len(word2)
-        
-                    
+        return len(word1) > len(word2) #ex: [apple, app] apple > app so not sorted and return false
