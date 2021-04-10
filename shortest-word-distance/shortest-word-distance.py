a0b8b123594b605c3556c2df0c5c9be24ba43047
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-10 17:44:20 -0500

    Time: 44 ms (90.53%), Space: 18.1 MB (21.40%) - LeetHub

---
+++ shortest-word-distance/shortest-word-distance.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def shortestDistance(self, wordsDict, word1, word2):
+        index1 = -1
+        index2 = -1
+        minimum = float("inf")
+        
+        for i in range(len(wordsDict)):
+            if word1 == wordsDict[i]:
+                index1 = i
+            if word2 == wordsDict[i]:
+                index2 = i
+            
+            if index1 != -1 and index2 != -1:
+                minimum = min(minimum, abs(index2 - index1))
+        return minimum
+        
