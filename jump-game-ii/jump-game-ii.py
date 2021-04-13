f3de2bab2627f73c0df3130dd5f00162db3ee3fe
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-12 20:10:46 -0500

    Time: 32 ms (78.78%), Space: 13.9 MB (99.25%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -4,13 +4,15 @@ class Solution:
         pos = 0
         jump = 0
         for i in range(len(nums) - 1):
+            # calculate the max dest from the index 
             maxdest = max(maxdest, i + nums[i])
             
+            # will get with min jump to reach destination
             if pos == i:
                 pos = maxdest
                 jump += 1
                 
-        if pos >= len(nums)- 1:
-            return jump
-        return 0
+        #if pos >= len(nums)- 1:
+        return jump
+        #return 0
         
