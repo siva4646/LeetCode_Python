3a681aceddcfc354ff8b6afa57e6079556c7fb92
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-13 10:15:38 -0500

    Time: 36 ms (95.58%), Space: 13.5 MB (99.82%) - LeetHub

--- string-compression/string-compression.py
+++ string-compression/string-compression.py
@@ -1,5 +1,11 @@
-class Solution:
-    def compress(self, chars: List[str]) -> int:
+class Solution(object):
+    def compress(self, chars):
+        if not chars:
+            return []
+        
+        if len(chars) == 1:
+            return 1
+        
         i = 0
         j = 0
         count = 1
@@ -10,58 +16,20 @@ class Solution:
             else:
                 chars[i] = chars[j]
                 i += 1
-                
                 if count > 1:
-                    for ch in str(count):
-                        chars[i] = ch
+                    for char in str(count):
+                        chars[i] = char
                         i += 1
-                        
-                count = 1
+                    count = 1    
             j += 1
-                
+            
         chars[i] = chars[j]
         i += 1
-
         if count > 1:
-            for ch in str(count):
-                chars[i] = ch
+            for char in str(count):
+                chars[i] = char
                 i += 1
+            count = 1
 
-        count = 0
-        
         return i
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # dic = {}
-        # result = []
-        # for char in chars:
-        #     if char not in dic:
-        #         dic[char] = 1
-        #     else:
-        #         dic[char] += 1
-        # for key, value in dic.items():
-        #     result.append(key)
-        #     result.append(str(value))
-        # return result
-        
-        
-        
         
