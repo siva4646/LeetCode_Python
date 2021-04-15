62a8558861fa92a4061e1762cc66001111789d4f
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-15 07:38:17 -0500

    Time: 24 ms (94.81%), Space: 14.1 MB (89.39%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,18 +1,18 @@
 # Understand the Recursive call clearly in video!
+
+# YOU NEED TO GIVE 3 SOLUTION N^2 , Bottom up with memoization T & S O(N) and iterative top down approach O(N) and O(1) V.V.V Important 
+
+
+
 class Solution:
     def fib(self, N: int) -> int:
-        cache = {}
-        def recur_fib(N):
-            if N in cache:
-                return cache[N]
-
-            if N < 2:
-                result = N
-            else:
-                result = recur_fib(N-1) + recur_fib(N-2)
-
-            # put result in cache for later reference.
-            cache[N] = result
-            return result
-
-        return recur_fib(N)   
+        if N <= 1:
+            return N
+        return self.memoize(N)
+    
+    def memoize(self, N):
+        cache = {0: 0, 1: 1}
+        
+        for i in range(2, N+1):
+            cache[i] = cache[i-1] + cache[i-2]
+        return cache[N]
