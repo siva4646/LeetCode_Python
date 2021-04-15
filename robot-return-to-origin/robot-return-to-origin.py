4811ea44295a3608299cfef0b14505b762c30946
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-15 09:44:25 -0500

    Time: 60 ms (57.63%), Space: 14.5 MB (21.38%) - LeetHub

---
+++ robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = 0
+        y = 0
+        
+        for move in moves:
+            if move == "R":
+                y += 1
+            elif move == "L":
+                y -= 1
+            elif move == "U":
+                x -= 1
+            elif move == "D":
+                x += 1
+        return x == y == 0
+            
+                
+        
