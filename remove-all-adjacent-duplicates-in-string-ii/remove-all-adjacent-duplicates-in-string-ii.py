151b951e2b69918dde3f1e0752d6bcaded2584c2
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-16 10:16:47 -0500

    Time: 52 ms (92.34%), Space: 14.9 MB (66.99%) - LeetHub

--- remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,35 +1,17 @@
-class Solution:
-    def removeDuplicates(self, s: str, k: int) -> str:
-        if not s or len(s) == 0 or not k:
-            return s
+class Solution(object):
+    def removeDuplicates(self, s, k):
+        if not s or len(s) == 0:
+            return ""
         
-        idx = 0
-        while idx < len(s):
-            if s[idx:idx+k] == s[idx] * k:
-                # for remove or bypass k character in the s.
-                s = s[:idx] + s[idx+k:]
-                idx = 0
+        stack = [["*",0]]
+
+        for char in s:
+            if char == stack[-1][0]:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
             else:
-                idx += 1
-        return s
-            
-        
-          
-        
-# MY CODE        
-#         count = 0
-#         flag = True
-#         result = ''
-#         for i in range(len(s)-1):
-#             if flag and s[i] == s[i+1] and count < k:
-#                 count += 1
-#                 substring = s[flag:count+1]
-#                 result = s.replace(substring, '')
-#             else:
-#                 count = 0
-#                 # flag = False
-#                 #i += 1 
-#         return result
-      
-        
-        
+                stack.append([char, 1])
+                
+        return "".join([char * times for char, times in stack])
+                
