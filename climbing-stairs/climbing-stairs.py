7efa54ecccf35c0818468d9f3e4160c8822d79d5
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-17 10:08:32 -0500

    Time: 24 ms (6.07%), Space: 13.3 MB (88.79%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,13 +1,22 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        
-        dp = [0] * (n + 1)
+# See Back to Back SWE Explanied so well!
+class Solution(object):
+    def climbStairs(self, n):
+#         return self.climb_Stairs(0, n)
+    
+#     def climb_Stairs(self, index, n):
+#         # base
+#         if index > n:
+#             return 0
+#         if index == n:
+#             return 1
+#         # recurse
+#         return self.climb_Stairs(index+1, n) + self.climb_Stairs(index+2, n)
+       
+        dp = [0] * (n+1)
         dp[0] = 1
         dp[1] = 1
         
-        for i in range(2, n + 1):
+        for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
         
