0fbfd224c21bd89b81f236e52fa66910c404ea4d
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-18 11:58:53 -0500

    Time: 56 ms (88.86%), Space: 15.8 MB (19.43%) - LeetHub

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,17 +1,22 @@
 class Solution(object):
     def findPairs(self, nums, k):
-        count = Counter(nums)
-        c_count = 0
+        hashMap = {}
+        count = 0
+        for num in nums:
+            if num not in hashMap:
+                hashMap[num] = 1
+            else:
+                hashMap[num] += 1
+                
         
-        for key, value in count.items():
+        for key, value in hashMap.items():
             if k == 0:
                 if value > 1:
-                    c_count += 1
+                    count += 1
             else:
                 total = key + k
-                if total in count:
-                    c_count += 1
-        return c_count 
+                if total in hashMap:
+                    count += 1
                     
-                
+        return count
         
