1d603f979d9b2f877d22fa2328ad87162bd76922
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-19 08:31:29 -0500

    Time: 120 ms (95.17%), Space: 15.4 MB (50.98%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,7 +1,8 @@
-class Solution(object):
-    def missingNumber(self, num):
-        missing = len(num)
-        for i, num in enumerate(num):
-            missing ^= i ^ num
-        return missing
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length = len(nums)
+        expected_sum = length * (length + 1) // 2
+        total_sum = sum(nums)
+        result = expected_sum - total_sum
+        return result
         
